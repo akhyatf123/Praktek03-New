29f2f2425ed75f78d7e2fb785a20a3d28f144a06
akhyat <akhyatf706@gmail.com>
2018-04-05 08:37:37 +0800

    menggunakan method set

--- src/praktek03/pkgnew/Aksi.java
+++ src/praktek03/pkgnew/Aksi.java
@@ -3,9 +3,10 @@ package praktek03.pkgnew;
 public class Aksi {
     public static void main(String[] args) {
          Smartphone e = new Smartphone();
-         e.jenis="Android";
-         e.merk="Samsung";
-         e.harga=900;
+         
+        e.setJenis("Android");
+        e.setMerk("Meizu M5S");
+        e.setHarga(9000);
          
          e.cetakInfo();
     }
