e4b9e826a1bcb4d706e90ed0cf575895bb29c4de
akhyat <akhyatf706@gmail.com>
2018-04-05 08:26:31 +0800

    instance object dan memanggil method

--- src/praktek03/pkgnew/Aksi.java
+++ src/praktek03/pkgnew/Aksi.java
@@ -1,5 +1,13 @@
 package praktek03.pkgnew;
 
 public class Aksi {
+    public static void main(String[] args) {
+         Smartphone e = new Smartphone();
+         e.jenis="Android";
+         e.merk="Samsung";
+         e.harga=900;
+         
+         e.cetakInfo();
+    }
     
 }
