b99f8d555670d0d3546fcfff347b5dda7c9b75a8
akhyat <akhyatf706@gmail.com>
2018-04-05 08:57:21 +0800

    attribut dan method

--- src/praktek03/pkgnew/iPhone.java
+++ src/praktek03/pkgnew/iPhone.java
@@ -1,6 +1,11 @@
 
 package praktek03.pkgnew;
 
-public class iPhone {
+public class iPhone extends Smartphone {
+    private String jenisIphone;
     
+    void iniIphone (){
+        System.out.println("Ulun handak Nukar IPHONE X tapi kada kawa");
+        
+            }
 }
